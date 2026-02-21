d712cc92ddc5c97a4f817b6b3bd1629d8b8cfcda
nimrod <nimrodium@protonmail.com>
2026-02-21 11:41:03 -0600

    fixed little merge conflict

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Space Sandbox";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+        # packages = with pkgs; [
+        #   SDL
+        #   zlib
+        # ];
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "kspire";
+          version = "0.1";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            SDL
+            zlib
+          ];
+          buildInputs = with pkgs; [ SDL ];
+          buildPhase = ''
+            make linux
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            ls *
+            false
+          '';
+          meta = with pkgs.lib; {
+            description = "Space Sandbox";
+            license = licenses.mit;
+            platforms = platforms.unix;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gnumake
+            SDL
+            zlib
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.sdl2-compat}/lib:$LD_LIBRARY_PATH
+            export PKG_CONFIG_PATH=${pkgs.sdl2-compat}/lib:$PKG_CONFIG_PATH
+            pkg-config --cflags --libs sdl2
+          '';
+        };
+      }
+    );
+}
